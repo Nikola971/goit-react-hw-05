135321df6d26b42d90ce0203fefa35aa96e24872
Nikola971 <mukola.milyan@outlook.com>
2024-02-14 15:39:51 +0200

    Update BackLink.jsx

--- src/components/BackLink/BackLink.jsx
+++ src/components/BackLink/BackLink.jsx
@@ -1,6 +1,6 @@
 import { IoArrowBack } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
-import css from '.';
+import css from './BackLink.module.css';
 
 export const BackLink = ({ to, children }) => {
   return (
